fd16251b1163222323cc1900de00b948f56d907c
Ilya Brovarnik <ilya.brovarnik@gmail.com>
2022-10-30 18:00:00 +0100

    init Spring and add html templates

---
+++ src/test/kotlin/com/example/wordsearcher/WordSearcherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.wordsearcher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordSearcherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
